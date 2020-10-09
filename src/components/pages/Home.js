33073f5dd4d076d7a2f232b4146cdb29b5288d2b
ThiagoCComelli <thiago.comelli@outlook.com>
2020-10-08 21:32:25 -0300

    adsense

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -7,6 +7,16 @@ function Home(props){
     return (
         <>
             <Main />
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+            <ins class="adsbygoogle"
+                    style="display:block"
+                    data-ad-client="ca-pub-4206059090363101"
+                    data-ad-slot="9220517382"
+                    data-ad-format="auto"
+                    data-full-width-responsive="true"></ins>
+            <script>
+                    (adsbygoogle = window.adsbygoogle || []).push({});
+            </script>
             <News />
         </>
     )
