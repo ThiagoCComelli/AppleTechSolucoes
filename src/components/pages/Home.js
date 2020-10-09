92fa866bc70321910d605e1dfa2d02f807662f43
ThiagoCComelli <thiago.comelli@outlook.com>
2020-10-09 10:46:06 -0300

    .

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,13 +2,11 @@ import React from 'react'
 import './Home.css'
 import Main from '../Main'
 import News from '../News'
-import Adsense from '../Adsense'
 
 function Home(){
     return (
         <>
             <Main />
-            <Adsense />
             <News />
         </>
     )
