12e100e49a1809d1d2a4cfecb1b76501c575a686
ThiagoCComelli <thiago.comelli@outlook.com>
2020-10-09 10:33:37 -0300

    .

--- src/components/Adsense.js
+++ src/components/Adsense.js
@@ -4,20 +4,20 @@ import React from 'react';
 export default class AdComponent extends React.Component {
   componentDidMount () {
     (window.adsbygoogle = window.adsbygoogle || []).push({});
+    console.log("foi")
   }
 
 render () {
     return (
         <>
-            <ins class="adsbygoogle"
-                    style={{ display: 'block' }}
-                    data-ad-client="ca-pub-4206059090363101"
-                    data-ad-slot="9220517382"
-                    data-ad-format="auto"
-                    data-full-width-responsive="true"></ins>
-            <script>
-                    (adsbygoogle = window.adsbygoogle || []).push({});
-            </script>
+            <ins className="adsbygoogle"
+              style={{ display: 'block' }}
+              data-ad-client="ca-pub-4206059090363101"
+              data-ad-slot="9220517382"
+              data-ad-format="auto"
+              data-full-width-responsive="true"
+              data-adtest="on">
+            </ins>
         </>
     )
   }
